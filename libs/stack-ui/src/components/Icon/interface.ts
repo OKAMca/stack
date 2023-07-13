c1c55f67b286d6901a5b0a06b80d5bf204d5b246
Jérôme Trottier <81482137+JeromeTrottier@users.noreply.github.com>
2023-07-13 10:54:32 -0400

    feat: extended svgprops for icon component (#15)

--- libs/stack-ui/src/components/Icon/interface.ts
+++ libs/stack-ui/src/components/Icon/interface.ts
@@ -1,7 +1,8 @@
 import type React from 'react'
+import type { SVGProps } from 'react'
 import type { TDefaultComponent } from '../../types/components'
 
-export interface TIconProps extends TDefaultComponent {
+export interface TIconProps extends TDefaultComponent, SVGProps<SVGSVGElement> {
   icon?: string | React.ReactNode
   children?: React.ReactNode
 }
