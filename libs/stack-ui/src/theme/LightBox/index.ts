678a8bfec28aa804a38eeaf17ea4ce968d0f8015
poclerson <cosmoscosmos993@gmail.com>
2023-09-18 13:44:22 +0000

    fix: test

--- libs/stack-ui/src/theme/LightBox/index.ts
+++ libs/stack-ui/src/theme/LightBox/index.ts
@@ -10,8 +10,19 @@ export const lightBoxContainer = tv({
 })
 
 export const lightBoxCloseBtn = tv({
-  base: `absolute xl:top-10 xl:right-20 lg:top-8 lg:right-8 z-10 top-4 right-4 w-[2.5rem]
-    h-[2.5rem] focus-ring-black`,
+  base: `
+    absolute
+    xl:top-10
+    xl:right-20
+    lg:top-8
+    lg:right-8
+    z-10
+    top-4
+    right-4
+    w-[2.5rem]
+    h-[2.5rem]
+    focus-ring-black
+  `,
 })
 
 export const lightBoxLabel = tv({
