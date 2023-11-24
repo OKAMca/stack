e6b36697672b715a6474b38ac9d40777cf5425e9
poclerson <cosmoscosmos993@gmail.com>
2023-11-24 17:19:22 +0000

    fix: img component story

--- libs/stack-ui/src/components/DirectusImg/index.tsx
+++ libs/stack-ui/src/components/DirectusImg/index.tsx
@@ -7,6 +7,8 @@ const imgDomain = process.env.NEXT_PUBLIC_IMG_DOMAIN ?? ''
 const DirectusImg = (props: TDirectusImageProps) => {
   const { fit, customTheme, thumbhash, description, width, height, id, filenameDownload, ...rest } = props
 
+  if (!id || !filenameDownload) return null
+
   const directusImageUrl = new URL(`/assets/${id}/${filenameDownload}`, `https://${imgDomain}`)
   directusImageUrl.searchParams.set('fit', fit ?? 'contain')
 
