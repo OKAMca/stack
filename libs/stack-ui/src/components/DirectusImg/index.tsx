08c0b752be1a1df3c8fcc59c01a53f226da2575b
poclerson <cosmoscosmos993@gmail.com>
2023-11-24 17:00:12 +0000

    fix: img and directusimg are different components

---
+++ libs/stack-ui/src/components/DirectusImg/index.tsx
@@ -0,0 +1,25 @@
+import { createPngDataUri } from 'unlazy/thumbhash'
+import Img from '../Img'
+import type TDirectusImageProps from './interface'
+
+const imgDomain = process.env.NEXT_PUBLIC_IMG_DOMAIN ?? ''
+
+const DirectusImg = (props: TDirectusImageProps) => {
+  const { fit, customTheme, thumbhash, description, width, height, id, filenameDownload, ...rest } = props
+
+  const directusImageUrl = new URL(`/assets/${id}/${filenameDownload}`, `https://${imgDomain}`)
+  directusImageUrl.searchParams.set('fit', fit ?? 'contain')
+
+  return (
+    <Img
+      alt={description ?? ''}
+      {...(thumbhash ? { blurDataURL: createPngDataUri(thumbhash), placeholder: 'blur' } : {})}
+      src={directusImageUrl.href}
+      {...rest}
+      width={width ?? undefined}
+      height={height ?? undefined}
+    />
+  )
+}
+
+export default DirectusImg
