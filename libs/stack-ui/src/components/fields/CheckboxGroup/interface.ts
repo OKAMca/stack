8c6f6b375e869b11424081a10d6f7f3c6dcb8380
poclerson <cosmoscosmos993@gmail.com>
2023-12-04 17:49:50 +0000

    fix: interface extends react aria checkbox group props

--- libs/stack-ui/src/components/fields/CheckboxGroup/interface.ts
+++ libs/stack-ui/src/components/fields/CheckboxGroup/interface.ts
@@ -1,7 +1,8 @@
 import type React from 'react'
+import type { CheckboxGroupProps } from 'react-stately'
 import type { TDefaultComponent } from '../../../types/components'
 
-export interface TCheckboxGroupProps extends TDefaultComponent {
+export interface TCheckboxGroupProps extends TDefaultComponent, CheckboxGroupProps {
   children: React.ReactNode
   isError?: boolean
   isDisabled?: boolean
