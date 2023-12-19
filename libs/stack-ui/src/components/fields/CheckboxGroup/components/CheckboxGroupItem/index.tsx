619202c47c171545cdac4d8d1771282746487e10
Pierre-Olivier Clerson <86995350+poclerson@users.noreply.github.com>
2023-12-19 16:46:51 -0500

    feat: add checkbox group component
    * chore: checkbox group component
    
    * fix: interface extends react aria checkbox group props
    
    * fix: checkbox group item

---
+++ libs/stack-ui/src/components/fields/CheckboxGroup/components/CheckboxGroupItem/index.tsx
@@ -0,0 +1,64 @@
+import Icon from 'libs/stack-ui/src/components/Icon'
+import Typography from 'libs/stack-ui/src/components/Typography'
+import { useCheckboxGroupCtx } from 'libs/stack-ui/src/providers/CheckboxGroup'
+import useThemeContext from 'libs/stack-ui/src/providers/Theme/hooks'
+import { useRef } from 'react'
+import { FocusRing, VisuallyHidden, useCheckboxGroupItem } from 'react-aria'
+import type { CheckboxGroupState } from 'react-stately'
+import type { TCheckboxGroupItemProps } from '../../interface'
+
+const ContextCheckboxGroupItem = (props: TCheckboxGroupItemProps & { state: CheckboxGroupState }) => {
+  const {
+    id,
+    ariaLabel,
+    icon,
+    state,
+    themeName = 'checkbox',
+    customTheme,
+    tokens,
+    isDisabled = false,
+    isError = false,
+    value,
+    errorMessage,
+    label,
+  } = props
+  const ref = useRef(null)
+  const { inputProps } = useCheckboxGroupItem(props, state, ref)
+  const { isSelected } = state
+
+  const checkBoxTokens = { ...tokens, selected: isSelected(value), isDisabled, isError }
+  const checkBoxContainerTheme = useThemeContext(`${themeName}.container`, checkBoxTokens, customTheme)
+  const checkBoxTheme = useThemeContext(`${themeName}.checkBox`, checkBoxTokens, customTheme)
+  const checkMarkTheme = useThemeContext(`${themeName}.checkMark`, checkBoxTokens, customTheme)
+  const checkMarkIconTheme = useThemeContext(`${themeName}.checkMarkIcon`, checkBoxTokens, customTheme)
+
+  return (
+    <div>
+      <FocusRing focusRingClass="has-focus-ring" within>
+        <label className={checkBoxContainerTheme} htmlFor={id} aria-label={ariaLabel} aria-disabled={isDisabled}>
+          <VisuallyHidden>
+            <input ref={ref} {...inputProps} />
+          </VisuallyHidden>
+          <div className={checkBoxTheme} aria-checked={isSelected(value)} role="checkbox" aria-labelledby={id}>
+            <div className={checkMarkTheme}>{icon && <Icon icon={icon} customTheme={checkMarkIconTheme} />}</div>
+          </div>
+          <Typography themeName={`${themeName}.label`} tokens={checkBoxTokens}>
+            {label}
+          </Typography>
+        </label>
+      </FocusRing>
+      {isError && errorMessage && (
+        <Typography themeName={`${themeName}.errorMessage`} tokens={checkBoxTokens}>
+          {errorMessage}
+        </Typography>
+      )}
+    </div>
+  )
+}
+
+const CheckboxGroupItem = (props: TCheckboxGroupItemProps) => {
+  const state = useCheckboxGroupCtx()
+  return state && <ContextCheckboxGroupItem state={state} {...props} />
+}
+
+export default CheckboxGroupItem
---
+++ libs/stack-ui/src/providers/CheckboxGroup/interface.ts
@@ -0,0 +1,6 @@
+import type React from 'react'
+import type { CheckboxGroupProps } from 'react-stately'
+
+export interface TCheckboxGroupContext extends CheckboxGroupProps {
+  children: React.ReactNode
+}
