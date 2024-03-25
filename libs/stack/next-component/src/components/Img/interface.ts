1c47f23fe2e091ce0fd53c63c48533a9de5a5ddf
RoshBJr <roshanjr3@gmail.com>
2024-03-25 20:35:34 +0000

    refactor: remove img component and image file

--- libs/stack/next-component/src/components/Img/interface.ts
+++
@@ -1,9 +0,0 @@
-import type { TDefaultComponent } from 'libs/stack/stack-ui/src/types/components'
-import type { ImageProps, StaticImageData } from 'next/image'
-
-type TImgProps = TDefaultComponent &
-  Omit<ImageProps, 'src'> & {
-    src: string | StaticImageData
-  }
-
-export default TImgProps
