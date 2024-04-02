3a3932738cf6ace7d5af72bd69e26c09ea3b44fc
Pierre-Olivier Clerson <86995350+poclerson@users.noreply.github.com>
2024-04-02 13:05:07 -0400

    feat: component accordion closed items should not be focusable via keyboard
    * feat: accordion content not focusable when acc. is closed/react aria integration for the acc
    
    * fix: with selection mode
    
    * feat: accessible accordion w/ accordion items
    
    * fix: default open works + full accordion accessibility
    
    * fix: use accordion state
    
    * chore: added exports

--- libs/stack/stack-ui/src/components/Accordion/interface.ts
+++ libs/stack/stack-ui/src/components/Accordion/interface.ts
@@ -1,16 +1,34 @@
-import type React from 'react'
+/* eslint-disable @typescript-eslint/naming-convention */
+import type { AccordionItemAriaProps } from '@react-aria/accordion'
+import type { ComponentType, ReactElement } from 'react'
+import type { ItemProps, TreeProps } from 'react-stately'
 import type { TToken } from '../../providers/Theme/interface'
-import type { TDefaultComponent } from '../../types/components'
+import type { TDefaultComponent, TTransition } from '../../types/components'
 
-export interface TAccordionProps extends TDefaultComponent<TAccordionTokens> {
-  id: string
-  title: string
-  icon?: React.ReactNode
-  ariaLabel: string
-  children: React.ReactNode
-  onClick?: (isOpen: boolean) => void
-  defaultIsOpen?: boolean
+type TAccordionDefaultComponent = Omit<TDefaultComponent<TAccordionTokens>, 'children'>
+
+export interface AccordionProps extends TreeProps<TAccordionItemProps> {
+  children: ReactElement<TAccordionItemProps> | ReactElement<TAccordionItemProps>[]
 }
+
+export interface TAccordionProps extends TAccordionDefaultComponent, AccordionProps {
+  id: string
+  TransitionAnimation?: ComponentType<TTransition>
+}
+
+export interface TAccordionItemProps extends ItemProps<TAccordionItemProps>, TAccordionDefaultComponent {
+  icon?: React.ReactNode
+  onOpenChange: (isOpen: boolean) => void
+  defaultOpen?: boolean
+  isOpen?: boolean
+}
+
+export type TAriaAccordionItemProps = TAccordionDefaultComponent &
+  Omit<AccordionItemAriaProps<TAccordionItemProps>, 'item'> & {
+    item: Omit<AccordionItemAriaProps<TAccordionItemProps>['item'], 'props'> & {
+      props?: TAccordionItemProps
+    }
+  }
 
 interface TAccordionTokens extends TToken {
   titleBold: boolean
