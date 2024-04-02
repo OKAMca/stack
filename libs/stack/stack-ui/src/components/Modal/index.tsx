85e791e21e7da9f9a87f15dea1426f5f65435461
Pierre-Olivier Clerson <86995350+poclerson@users.noreply.github.com>
2024-04-02 13:36:53 -0400

    fix: lightbox component accessibility + modal integrates react aria
    * fix: added aria-modal + replaced useButton by useOverlayTrigger
    
    * fix: lightbox component accessibility + modal integrates react aria
    
    * fix: full lightbox & modal accessibility
    
    * chore: updated example in demo
    
    * fix: remove console import

--- libs/stack/stack-ui/src/components/Modal/index.tsx
+++ libs/stack/stack-ui/src/components/Modal/index.tsx
@@ -1,39 +1,29 @@
 'use client'
 
-/* eslint-disable react-hooks/exhaustive-deps */
-import { OverlayContainer } from '@react-aria/overlays'
-import { useCallback, useEffect, useState } from 'react'
+import { OverlayContainer } from 'react-aria'
 import ModalTransition from '../../transitions/ModalTransition'
-import Dialog from './Dialog'
+import ModalDialog from './components/ModalDialog'
+import ModalOverlay from './components/ModalOverlay'
 import type { TModalProps } from './interface'
 
 const Modal = (props: TModalProps) => {
-  const [shouldRender, setShouldRender] = useState(false)
-  const { children, state, onCloseCallBack, transitionComponent: TransitionComponent = ModalTransition } = props
-
-  const handleClose = useCallback(() => {
-    state.close()
-    onCloseCallBack?.()
-  }, [state.close, onCloseCallBack])
-
-  useEffect(() => {
-    if (!state.isOpen && shouldRender) {
-      return
-    }
-
-    setShouldRender(true)
-  }, [state.isOpen])
-
-  if (!shouldRender) {
-    return null
-  }
+  const {
+    children,
+    transitionComponent: TransitionComponent = ModalTransition,
+    state,
+    themeName,
+    tokens,
+    ...rest
+  } = props
 
   return (
     <OverlayContainer>
       <TransitionComponent isVisible={state.isOpen}>
-        <Dialog isOpen={state.isOpen} onClose={handleClose} isDismissable>
-          {children}
-        </Dialog>
+        <ModalOverlay themeName={themeName} tokens={tokens} state={state} {...rest}>
+          <ModalDialog themeName={themeName} tokens={tokens} {...rest}>
+            {children}
+          </ModalDialog>
+        </ModalOverlay>
       </TransitionComponent>
     </OverlayContainer>
   )
