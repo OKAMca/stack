3a5b774713a477a311d43c3381a06b411d9efddc
Jérôme Trottier <jerome.trottier@hotmail.ca>
2024-06-13 15:32:20 +0000

    fix: missing a11y pagination features

--- libs/stack/stack-ui/src/components/Alerts/interface.ts
+++ libs/stack/stack-ui/src/components/Alerts/interface.ts
@@ -1,6 +1,7 @@
 import type React from 'react'
 import type * as SwiperModules from 'swiper/modules'
 import type { SwiperClass, SwiperProps } from 'swiper/react'
+import type { A11yOptions } from 'swiper/types'
 import type { TDefaultComponent } from '../../types/components'
 import type { TButtonProps } from '../Button/interface'
 import type { LightboxProps } from '../Lightbox/interface'
@@ -22,11 +23,23 @@ export interface TAlertsPaginationProps extends TDefaultComponent {
   activeIndex: number
   alerts: TAlertsItem[]
   controller?: SwiperClass
+  paginationGroupLabel?: string
+}
+
+export interface TAlertsPaginationBulletProps extends TDefaultComponent {
+  activeIndex: number
+  alerts: TAlertsItem[]
+  controller?: SwiperClass
+  index: number
+}
+interface TCustomA11yOptions extends A11yOptions {
+  paginationGroupLabel?: string
 }
 
 export interface TAlertsComponentProps
   extends Omit<TDefaultComponent, 'children'>,
-    Omit<SwiperProps, 'children' | 'modules'> {
+    Omit<SwiperProps, 'children' | 'modules' | 'a11y'> {
+  a11y?: TCustomA11yOptions
   alerts: TAlertsItem[]
   children?: (props: TAlertsItemProps) => React.ReactNode
   modules?: (keyof typeof SwiperModules)[]
