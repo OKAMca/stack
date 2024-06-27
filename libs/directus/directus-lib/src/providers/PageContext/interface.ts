432dbeb12743ef6860f092084332c03a89223c6e
mykimd <mykim@okam.ca>
2024-06-27 17:59:06 +0000

    feat: change interface

--- libs/directus/directus-lib/src/providers/PageContext/interface.ts
+++ libs/directus/directus-lib/src/providers/PageContext/interface.ts
@@ -2,19 +2,17 @@ import type React from 'react'
 
 export type TPageContext<T extends object> = {
   title: string
-  collection: string
+  belongToCollection: string
   slug?: string
   path?: string
-  route?: string
   state?: T
 }
 
 export interface TPageProviderProps<T extends object> {
   children: React.ReactNode
   title: string
-  collection: string
+  belongToCollection: string
   slug?: string
   path?: string
-  route?: string
   state?: T
 }
