5b4152611d0b6ea3e4e7c65f9e7c74ec51deded4
Jérôme Trottier <jerome.trottier@hotmail.ca>
2024-07-10 20:23:57 +0000

    fix(directus-flexible-content): add stack-ui and directus block to external deps

--- libs/directus/directus-flexible-content/src/index.ts
+++ libs/directus/directus-flexible-content/src/index.ts
@@ -1,2 +1,4 @@
-export * from './lib/components'
-export * from './lib/functions'
+export { FlexibleEditorContent } from './lib/components'
+export { RenderNodes } from './lib/components'
+export type { TRenderingNodes } from './lib/components/nodes/types'
+export type { JSONContent } from './lib/functions/types'
