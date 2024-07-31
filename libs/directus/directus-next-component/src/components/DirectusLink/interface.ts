15befc862a28bbea8c48819a7ccf1fc24753b38b
poclerson <pierre-olivier@okam.ca>
2024-07-31 13:51:25 +0000

    fix(directus-next-component): add aria attributes to directus link props

--- libs/directus/directus-next-component/src/components/DirectusLink/interface.ts
+++ libs/directus/directus-next-component/src/components/DirectusLink/interface.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/naming-convention */
 import type { TDefaultComponent, TAnchorProps } from '@okam/stack-ui'
-import type { ComponentType } from 'react'
+import type { AriaAttributes, ComponentType } from 'react'
 import type { TLinks } from '../../types/links'
 
-export interface TDirectusLinkProps extends Omit<TDefaultComponent, 'children'>, Omit<Partial<TLinks>, 'tokens'> {
+export interface TDirectusLinkProps
+  extends Omit<TDefaultComponent, 'children'>,
+    Omit<Partial<TLinks>, 'tokens'>,
+    AriaAttributes {
   componentsConfig?: TDirectusLinkComponentsConfig
   propsConfig?: TDirectusLinkPropsConfig
 }
