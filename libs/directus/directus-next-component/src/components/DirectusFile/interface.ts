3940f5e9bf0262eca4b786ef1dfe2dcaed1e841e
poclerson <pierre-olivier@okam.ca>
2024-09-17 15:53:44 +0000

    fix(directus-next-component): directusimg supports all directus search params

--- libs/directus/directus-next-component/src/components/DirectusFile/interface.ts
+++ libs/directus/directus-next-component/src/components/DirectusFile/interface.ts
@@ -1,7 +1,9 @@
 import type { TDefaultComponent } from '@okam/stack-ui'
 import type { TFiles } from '../../types/files'
+import type { TDirectusImgProps } from '../DirectusImg/interface'
+import type { TDirectusVideoProps } from '../DirectusVideo/interface'
 
-export interface TDirectusFileProps extends TFiles, TDefaultComponent {
+export interface TDirectusFile extends TFiles {
   /**
    * Serves to fall back to different parts of the URLs. Some properties have default values:
    * - hostname: `NEXT_PUBLIC_IMG_DOMAIN`
@@ -10,3 +12,5 @@ export interface TDirectusFileProps extends TFiles, TDefaultComponent {
    */
   baseUrl?: URL
 }
+
+export type TDirectusFileProps = TFiles & TDefaultComponent & TDirectusImgProps & TDirectusVideoProps
