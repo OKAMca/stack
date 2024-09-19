c0fa8a6c88a931c7271554bda8183c30b61f8a00
David Paquet Pitts <david.paquet@okam.ca>
2024-09-19 13:23:26 -0400

    style: add trailing comma to function parameter

--- libs/directus/directus-next-component/src/lib/directus-next-rewrite.ts
+++ libs/directus/directus-next-component/src/lib/directus-next-rewrite.ts
@@ -4,7 +4,7 @@ import type { NextRequest, NextResponse } from 'next/server'
 export function directusRouteRouter(
   request: NextRequest,
   config: Parameters<typeof originalDirectusRouteRouter>[1],
-  NextResponse: Parameters<typeof originalDirectusRouteRouter>[2]
+  NextResponse: Parameters<typeof originalDirectusRouteRouter>[2],
 ) {
   return originalDirectusRouteRouter(request, config, NextResponse)
 }
