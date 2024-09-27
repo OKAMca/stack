5ffa03aa746fccd0a28e0fb7cf55da1096c14e10
Jérôme Trottier <jerome.trottier@hotmail.ca>
2024-09-27 20:45:12 +0000

    feat(directus-flexible-content): added support for marks and inline blocks

--- libs/directus/directus-flexible-content/src/lib/components/FlexibleEditorContent/index.tsx
+++ libs/directus/directus-flexible-content/src/lib/components/FlexibleEditorContent/index.tsx
@@ -1,7 +1,7 @@
 import type { TBlockSerializerConfig } from '@okam/directus-block'
 import { BlockDispatcher } from '@okam/directus-block/server'
 import type { TDefaultComponent } from '@okam/stack-ui'
-import { Node } from '@tiptap/core'
+import { Mark, Node } from '@tiptap/core'
 import { injectDataIntoContent } from '../../functions'
 import type { JSONContent, Extensions, EditorNodes } from '../../functions/types'
 import type { RelationBlockProps } from '../../types'
@@ -14,42 +14,69 @@ interface FlexibleEditorContentProps extends TDefaultComponent {
   editorNodes?: EditorNodes[] | undefined | null
   serializers?: Extensions
   config?: TBlockSerializerConfig
+  relationMarksConfig?: TBlockSerializerConfig
   nodes?: TRenderingNodes
   remappedAttributes?: Record<string, string>
 }
 
 const FlexibleEditorContent = (props: FlexibleEditorContentProps) => {
-  const { jsonContent, editorNodes, serializers, nodes, config, themeName, tokens, customTheme, remappedAttributes } =
-    props
+  const {
+    jsonContent,
+    editorNodes,
+    serializers,
+    nodes,
+    config,
+    relationMarksConfig,
+    themeName,
+    tokens,
+    customTheme,
+    remappedAttributes,
+  } = props
 
   const content = injectDataIntoContent(editorNodes, jsonContent)
-
   // `.slice(0)` to clone the extensions array
   const effectiveSerializers = serializers ?? [...extensions] ?? []
+
+  const renderRelationBlock = (block: RelationBlockProps, blockDispatcherConfig?: TBlockSerializerConfig) => {
+    const { attrs } = block
+    const blockKey = `${attrs?.collection?.replace('related_', '')}_id`
+    if (attrs?.data) {
+      if (!attrs?.data?.blocks) {
+        const properBlock = { collection: attrs.collection, item: { ...attrs?.data } }
+        return <BlockDispatcher key={JSON.stringify(properBlock)} block={properBlock} config={blockDispatcherConfig} />
+      }
+      const items = attrs?.data?.blocks
+      const blocks = items?.map((item) => {
+        const properItem = item?.[blockKey] as Record<string, unknown>
+        return { item: properItem, collection: attrs?.collection }
+      })
+      return <BlockDispatcher key={JSON.stringify(items)} blocks={blocks} config={blockDispatcherConfig} />
+    }
+    return null
+  }
 
   const relationBlockSerializer = Node.create({
     name: 'relation-block',
     renderHTML: () => ['relation-block'],
+    render: (block: RelationBlockProps) => renderRelationBlock(block, config),
+  })
+
+  const relationInlineBlockSerializer = Node.create({
+    name: 'relation-inline-block',
+    renderHTML: () => ['relation-inline-block'],
+    render: (block: RelationBlockProps) => renderRelationBlock(block, config),
+  })
+
+  const relationMarkSerializer = Mark.create({
+    name: 'relation-mark',
+    type: 'relation-mark',
+    renderHTML: () => ['relation-mark'],
     render: (block: RelationBlockProps) => {
-      const { attrs } = block
-      const blockKey = `${attrs?.collection?.replace('related_', '')}_id`
-      if (attrs?.data) {
-        if (!attrs?.data?.blocks) {
-          const properBlock = { collection: attrs.collection, item: { ...attrs?.data } }
-          return <BlockDispatcher key={JSON.stringify(properBlock)} block={properBlock} config={config} />
-        }
-        const items = attrs?.data?.blocks
-        const blocks = items?.map((item) => {
-          const properItem = item?.[blockKey] as Record<string, unknown>
-          return { item: properItem, collection: attrs?.collection }
-        })
-        return <BlockDispatcher key={JSON.stringify(items)} blocks={blocks} config={config} />
-      }
-      return null
+      return renderRelationBlock(block, relationMarksConfig)
     },
   })
 
-  effectiveSerializers.push(relationBlockSerializer)
+  effectiveSerializers.push(relationBlockSerializer, relationInlineBlockSerializer, relationMarkSerializer)
 
   if (!content) {
     return null
