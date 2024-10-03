54824eee5469aed09668e3dc0f51c440859e29e2
Marie-Maxime Tanguay <marie-maxime@okam.ca>
2024-10-03 13:51:32 -0400

    fix: test

--- libs/stack/logger/src/lib/logger.spec.ts
+++ libs/stack/logger/src/lib/logger.spec.ts
@@ -1,12 +1,33 @@
 import { logger } from './logger'
 
 describe('logger', () => {
-  it('should work', () => {
+  it('should have required methods and properties', () => {
     expect(logger).toBeDefined()
-    expect(logger.log).toBeDefined()
-    expect(logger.setLogger).toBeDefined()
-    expect(logger.setLogger).toBeInstanceOf(Function)
     expect(logger.log).toBeInstanceOf(Function)
-    expect(logger.log('test')).toBeUndefined()
+    expect(logger.setLogger).toBeInstanceOf(Function)
+  })
+
+  it('should log messages at different levels', () => {
+    const mockLog = jest.fn()
+    logger.setLogger(mockLog)
+
+    logger.log('Info message', 'info', { key: 'value' })
+    expect(mockLog).toHaveBeenCalledWith('Info message', 'info', { key: 'value' })
+
+    logger.log('Warning message', 'warn')
+    expect(mockLog).toHaveBeenCalledWith('Warning message', 'warn', undefined)
+
+    logger.log('Error message', 'error')
+    expect(mockLog).toHaveBeenCalledWith('Error message', 'error', undefined)
+  })
+
+  it('should allow setting a custom logger', () => {
+    const customLogger = {
+      log: jest.fn(),
+    }
+    logger.setLogger(customLogger.log)
+
+    logger.log('Test message', 'info')
+    expect(customLogger.log).toHaveBeenCalledWith('Test message', 'info', undefined)
   })
 })
