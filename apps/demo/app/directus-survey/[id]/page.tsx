05b1040702709a4f50fce92f687cc7a912b7c516
Jérôme Trottier <jerome.trottier@hotmail.ca>
2024-10-04 17:35:01 +0000

    fix(demo): remove cache for survey demo

--- apps/demo/app/directus-survey/[id]/page.tsx
+++ apps/demo/app/directus-survey/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { initDirectusQuery } from '@okam/directus-query'
 import { DirectusSurvey } from '@okam/stack-ui'
 import { FormConfigDocument, FormConfigFragmentDoc, useFragment } from 'libs/directus-data-query/survey'
+import { unstable_noStore as noStore } from 'next/cache'
 
 export default async function Index({ params }: { params: { id: string } }) {
+  noStore()
   const token = process.env.DIRECTUS_FORM_API_TOKEN
   const directusUrl = process.env.DIRECTUS_FORM_URL
   const queryClient = initDirectusQuery(`${directusUrl}/graphql`, {
