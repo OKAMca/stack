841cbb0d072c14bba77a7f8e4b0dc0e93a66e440
Marie-Maxime Tanguay <marie-maxime@okam.ca>
2024-10-28 20:04:31 +0000

    fix: missing logger

--- libs/directus/directus-next-component/src/server.ts
+++ libs/directus/directus-next-component/src/server.ts
@@ -5,7 +5,7 @@ export { default as DirectusFile } from './components/DirectusFile'
 export { default as DirectusImg } from './components/DirectusImg'
 export { default as DirectusLink } from './components/DirectusLink'
 export { default as DirectusVideo } from './components/DirectusVideo'
-
+export { logger as DirectusNextComponent } from './logger'
 export { default as useDirectusFile } from './hooks/directus-file'
 export { default as useDirectusLink } from './hooks/directus-link'
 export { default as useNavigationItems } from './hooks/navigation-items'
--- libs/directus/directus-query/src/lib/request.tsx
+++ libs/directus/directus-query/src/lib/request.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 import { QueryCache, QueryClient } from '@tanstack/react-query'
 import { GraphQLClient } from 'graphql-request'
+import { logger } from '../logger'
 
 const GRAPHQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_URL as string
 const GRAPHQL_ENDPOINT_ADMIN = process.env.NEXT_GRAPHQL_URL_ADMIN as string
@@ -29,7 +30,7 @@ export const graphqlRequestAdmin = new GraphQLClient(GRAPHQL_ENDPOINT_ADMIN, {
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({
     onError: (error) => {
-      console.error(error)
+      logger.log('QueryClient Error', 'error', { error })
     },
   }),
   defaultOptions: {
