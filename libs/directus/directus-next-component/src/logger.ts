156558229f9096f283636c58e5f45598ee921465
Marie-Maxime Tanguay <11244863+marie-maxime@users.noreply.github.com>
2024-11-05 15:32:14 -0500

    feat: add factory for Logger (#214)
    feat: add factory for Logger

---
+++ libs/directus/directus-next-component/src/logger.ts
@@ -0,0 +1,3 @@
+import { createLogger } from '@okam/logger'
+
+export const logger = createLogger('[DirectusNextComponent]')
--- libs/directus/directus-query/src/index.ts
+++ libs/directus/directus-query/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/hooks'
 export * from './lib/query'
+export { logger as DirectusQueryLogger } from './logger'
 export { default as initDirectusQuery } from './lib/init'
