06b43b816b1aefc77c50d8abee744c93bf90c1e6
poclerson <pierre-olivier@okam.ca>
2025-01-30 12:35:00 -0500

    fix(directus-data-query): added test query for block wysiwyg

--- apps/directus-data-query/src/lib/gql/generated/gql.ts
+++ apps/directus-data-query/src/lib/gql/generated/gql.ts
@@ -11,8 +11,11 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * 3. It does not support dead code elimination, so it will add unused operations.
  *
  * Therefore it is highly recommended to use the babel or swc plugin for production.
+ * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
  */
 const documents = {
+    "fragment BlockSettings on block_settings {\n  variant\n  tokens\n}": types.BlockSettingsFragmentDoc,
+    "fragment BlockWysiwygs on block_wysiwygs {\n  id\n  content\n  settings {\n    ...BlockSettings\n  }\n}\n\nquery BlockWysiwygs($id: ID!) {\n  block_wysiwygs_by_id(id: $id) {\n    ...BlockWysiwygs\n  }\n}": types.BlockWysiwygsFragmentDoc,
     "fragment DirectusFiles on directus_files {\n  id\n  filename_disk\n  filename_download\n  title\n  type\n  width\n  height\n  duration\n  embed\n  description\n  focal_point_x\n  focal_point_y\n  caption\n  thumbhash\n}": types.DirectusFilesFragmentDoc,
     "query BlockFiles($id: ID!) {\n  block_files_by_id(id: $id) {\n    files {\n      directus_files_id {\n        ...DirectusFiles\n      }\n    }\n  }\n}": types.BlockFilesDocument,
     "query Pages {\n  pages {\n    title\n    translations {\n      languages_code {\n        code\n      }\n      flexible_editor\n      editor_nodes {\n        id\n        pages_translations_id {\n          languages_code {\n            code\n          }\n        }\n        collection\n        item {\n          ... on block_quote {\n            content\n            id\n            subtitle\n            title\n          }\n          ... on block_button {\n            color\n            date_created\n            date_updated\n            external_url\n            id\n            label\n            sort\n            type\n            variant\n          }\n          ... on related_block_faqs {\n            id\n            blocks {\n              id\n              block_faqs_id {\n                alignment\n                faqs\n                headline\n                id\n                title\n              }\n            }\n          }\n          ... on links {\n            id\n            label\n          }\n        }\n      }\n    }\n  }\n}": types.PagesDocument,
@@ -32,6 +35,14 @@ const documents = {
  */
 export function graphql(source: string): unknown;
 
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "fragment BlockSettings on block_settings {\n  variant\n  tokens\n}"): (typeof documents)["fragment BlockSettings on block_settings {\n  variant\n  tokens\n}"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "fragment BlockWysiwygs on block_wysiwygs {\n  id\n  content\n  settings {\n    ...BlockSettings\n  }\n}\n\nquery BlockWysiwygs($id: ID!) {\n  block_wysiwygs_by_id(id: $id) {\n    ...BlockWysiwygs\n  }\n}"): (typeof documents)["fragment BlockWysiwygs on block_wysiwygs {\n  id\n  content\n  settings {\n    ...BlockSettings\n  }\n}\n\nquery BlockWysiwygs($id: ID!) {\n  block_wysiwygs_by_id(id: $id) {\n    ...BlockWysiwygs\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
