53bbd4a5b69ce55fe0150dc07ca5cf62f0811a54
poclerson <pierre-olivier@okam.ca>
2025-02-04 13:11:15 -0500

    test(demo): remove unused example call to usePageSettings

--- apps/demo/app/[locale]/pages/[id]/page.tsx
+++ apps/demo/app/[locale]/pages/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { usePageSettings } from '@okam/directus-next/server'
-import type { PageFragment } from 'apps/directus-data-query/src'
-import { PageByIdDocument, PageByIdWithoutFragmentDocument } from 'apps/directus-data-query/src'
+import { PageByIdDocument } from 'apps/directus-data-query/src'
 import { directusConfig } from '../../../../middleware'
 
 export default async function Page(props: { params: { id: string; locale: string } }) {
@@ -10,12 +9,13 @@ export default async function Page(props: { params: { id: string; locale: string
     config: directusConfig,
   })
 
-  const dataWithoutFragment = await usePageSettings<PageFragment, 'pages_by_id', typeof props.params>({
-    document: PageByIdWithoutFragmentDocument,
-    variables: props.params,
-    config: directusConfig,
-  })
-  const { translations, page_settings } = dataWithoutFragment ?? {}
+  // If the query has no main fragment, the type parameters must be passed.
+  // const dataWithoutFragment = await usePageSettings<PageFragment, 'pages_by_id', typeof props.params>({
+  //   document: PageByIdWithoutFragmentDocument,
+  //   variables: props.params,
+  //   config: directusConfig,
+  // })
+  // const { translations, page_settings } = dataWithoutFragment ?? {}
 
   return (
     <div>
