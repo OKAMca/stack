f03c0804c37ea5b35a2a74ef724f19cd2392c895
Marie-Maxime Tanguay <marie-maxime@okam.ca>
2025-03-20 10:42:35 -0400

    fix: rhf wrapper

--- libs/stack/stack-ui/src/components/fields/TextArea/index.tsx
+++ libs/stack/stack-ui/src/components/fields/TextArea/index.tsx
@@ -6,7 +6,8 @@ import { FocusRing } from '@react-aria/focus'
 import { isEmpty } from 'radashi'
 import React, { useRef } from 'react'
 import { useTextField } from 'react-aria'
-import { useFormContext, get } from 'react-hook-form'
+import type { RegisterOptions } from 'react-hook-form'
+import { Controller, useFormContext } from 'react-hook-form'
 import useThemeContext from '../../../providers/Theme/hooks'
 import type { TToken } from '../../../providers/Theme/interface'
 import { useTranslation } from '../../../providers/Translation'
@@ -43,7 +44,7 @@ const TextArea = <T extends TToken>(props: TTextInputProps<T>) => {
 
   return (
     <div>
-      <div className={wrapper} aria-disabled={disabled}>
+      <div className={wrapper} aria-disabled={disabled ? 'true' : 'false'}>
         {label && (
           <label className={labelTheme} {...labelProps}>
             {label}
@@ -81,26 +82,105 @@ const TextArea = <T extends TToken>(props: TTextInputProps<T>) => {
   )
 }
 
-export const ReactHookFormTextArea = (props: TTextInputProps) => {
-  const { name, required, minLength = 0, maxLength = 99999999, validation } = props
-  const { register, formState } = useFormContext()
-  const error: Error = get(formState.errors, name)
-  const errMsg = error?.message ?? undefined
+export const ReactHookFormTextArea = ({
+  name,
+  label,
+  rules,
+  tokens,
+  ariaLabel,
+  defaultValue = '',
+  placeholder = '',
+  themeName = 'textarea',
+  isReadOnly = false,
+  minLength,
+  maxLength,
+  required,
+  isRequired,
+  onChange,
+  onBlur,
+  children = null,
+  ...rest
+}: TTextInputProps & { rules: RegisterOptions }) => {
+  const { control } = useFormContext()
   const { t } = useTranslation()
-  const { ref: refCallback, ...rest } = register(name, {
-    required: required ? (t('FORM.ERROR.REQUIRED') ?? 'required') : false,
-    minLength: {
-      value: minLength,
-      message: t('FORM.ERROR.MIN_LENGTH', { length: minLength }),
-    },
-    maxLength: {
-      value: maxLength,
-      message: t('FORM.ERROR.MAX_LENGTH', { length: minLength }),
-    },
-    ...validation,
-  })
 
-  return <TextArea fieldRef={refCallback} {...rest} {...props} isError={!isEmpty(errMsg)} errorMessage={errMsg} />
+  const getAriaFields = () => {
+    const ariaFields = Object.entries(rest).filter(([key, _value]) => key.startsWith('aria-'))
+    return Object.fromEntries(ariaFields)
+  }
+
+  const ruleMerged = {
+    required: (required ?? isRequired) ? (t('FORM.ERROR.REQUIRED') ?? 'required') : false,
+    ...(minLength != null && {
+      minLength: {
+        value: minLength,
+        message: t('FORM.ERROR.MIN_LENGTH', { length: minLength }),
+      },
+    }),
+    ...(maxLength != null && {
+      maxLength: {
+        value: maxLength,
+        message: t('FORM.ERROR.MAX_LENGTH', { length: maxLength }),
+      },
+    }),
+    ...rules,
+  }
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={ruleMerged}
+      disabled={rules?.disabled}
+      defaultValue={defaultValue}
+      render={({ field, fieldState }) => {
+        const { ref, ...fieldProps } = field
+        const isError = !isEmpty(fieldState.error)
+
+        const validityField = {
+          isDisabled: field.disabled ?? false,
+          isRequired: rules?.required === true || rules?.required === 'required',
+          isInvalid: fieldState.invalid,
+          isError,
+          isReadOnly,
+        }
+
+        const inputTokens = {
+          ...tokens,
+          ...validityField,
+        }
+
+        return (
+          <TextArea
+            {...fieldProps}
+            {...validityField}
+            {...getAriaFields()}
+            fieldRef={ref}
+            name={name}
+            placeholder={placeholder}
+            themeName={themeName}
+            label={label}
+            tokens={inputTokens}
+            ariaLabel={ariaLabel}
+            isDisabled={field.disabled}
+            isRequired={rules?.required === true || rules?.required === 'required'}
+            isInvalid={fieldState.invalid}
+            errorMessage={fieldState.error?.message}
+            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+              field.onChange(e)
+              onChange?.(e)
+            }}
+            onBlur={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+              field.onBlur()
+              onBlur?.(e)
+            }}
+          >
+            {children}
+          </TextArea>
+        )
+      }}
+    />
+  )
 }
 
 export default TextArea
