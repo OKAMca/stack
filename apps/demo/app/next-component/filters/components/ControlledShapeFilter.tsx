3423b8398578c1d4d7b9c19214503f0654c1ab7f
poclerson <pierre-olivier@okam.ca>
2025-03-24 14:49:26 -0400

    fix(next-component): controlled/uncontrolled states are always synced

--- apps/demo/app/next-component/filters/components/ControlledShapeFilter.tsx
+++ apps/demo/app/next-component/filters/components/ControlledShapeFilter.tsx
@@ -2,24 +2,47 @@
 
 import { Filter } from '@okam/next-component'
 import { Button } from '@okam/stack-ui'
+import { useState } from 'react'
+import type { Key } from 'react-stately'
 import { Item } from 'react-stately'
-import { useSet } from 'react-use'
+
+const shapesData = ['circle', 'square', 'triangle']
 
 const ControlledShapeFilter = () => {
-  const shapesData = ['circle', 'square', 'triangle']
-  const [shapes, shapesActions] = useSet<string>(new Set())
+  const [shapes, setShapes] = useState<Key[]>([])
+
+  function toggleShape(shape: Key) {
+    if (shapes.includes(shape)) {
+      setShapes(shapes.filter((s) => s !== shape))
+    } else {
+      setShapes([...shapes, shape])
+    }
+  }
 
   return (
     <div>
       {shapesData.map((shape) => (
-        <Button key={shape} handlePress={() => shapesActions.toggle(shape)}>
+        <Button
+          key={shape}
+          handlePress={() => {
+            toggleShape(shape)
+          }}
+        >
           {shape}
         </Button>
       ))}
-      <Filter id="shape" label="Shape" selectedKeys={shapes}>
-        <Item key="circle">Circle</Item>
-        <Item key="square">Square</Item>
-        <Item key="triangle">Triangle</Item>
+      <Filter
+        id="shape"
+        label="Shape"
+        selectedKeys={shapes}
+        onSelectionChange={(keys) => {
+          const keysArray = Array.from(keys)
+          setShapes(keysArray)
+        }}
+      >
+        {shapesData.map((shape) => (
+          <Item key={shape}>{shape}</Item>
+        ))}
       </Filter>
     </div>
   )
