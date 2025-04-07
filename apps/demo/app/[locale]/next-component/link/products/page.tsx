52461410e556b68d2b6e26bd085573c4ed53b16c
poclerson <pierre-olivier@okam.ca>
2025-04-07 14:08:04 -0400

    docs(demo): next component Link example in next app

--- apps/demo/app/[locale]/next-component/link/products/page.tsx
+++ apps/demo/app/[locale]/next-component/link/products/page.tsx
@@ -1,16 +1,13 @@
 import { Link } from '@okam/next-component'
 import { Box, Typography } from '@okam/stack-ui'
 
-export default async function Page(props: { params: { locale: string } }) {
-  const { params } = props
-  const { locale } = params
-
+export default async function Page() {
   return (
     <Box>
       <Box customTheme="h-48 bg-gray-300 justify-center items-center flex">
         <Typography tokens={{ size: 'h1' }}>Products home page</Typography>
       </Box>
-      <Link href={`/${locale}/next-component/link/products/1`}>Products 1</Link>
+      <Link href="/next-component/link/products/1">Products 1</Link>
     </Box>
   )
 }
