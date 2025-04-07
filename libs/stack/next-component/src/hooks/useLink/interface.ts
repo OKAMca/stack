c1896f9138cf21bd1f8f761d0cf039aa4994040b
poclerson <pierre-olivier@okam.ca>
2025-04-07 14:05:54 -0400

    feat(next-component): useLink hook

--- libs/stack/next-component/src/hooks/useLink/interface.ts
+++ libs/stack/next-component/src/hooks/useLink/interface.ts
@@ -1,28 +1,25 @@
 import type { LinkProps as NextLinkProps } from 'next/link'
 import type { ReadonlyURLSearchParams } from 'next/navigation'
 
-export type TScrollProp =
-  | NextLinkProps['scroll']
-  /**
-   * Scroll to the top of the page when the link is clicked.
-   */
-  | 'top'
-
 export interface TLink extends Omit<NextLinkProps, 'scroll' | 'as'> {
   /**
    * @default true
+   * - `true`: Scrolls to the top of the clicked anchor (default Next.js behavior)
+   * - `false`: Prevents any automatic scrolling
+   * - `'top'`: Always scrolls to the top of the page, regardless of anchor
    */
-  scroll?: TScrollProp | undefined
+  scroll?: 'top' | boolean
   urlDecorator?: NextLinkProps['as']
   /**
    * Used for the automatic scroll when `scroll = 'top'`
-   * @default 'instant'
+   * @default instant
    */
   behavior?: ScrollBehavior
-}
-
-export interface TLinkOptions extends TLink {
   onPathnameChange?: (pathname: string) => void
   onSearchParamsChange?: (searchParams: ReadonlyURLSearchParams) => void
   onHashChange?: (hash: string) => void
+}
+
+export interface TUseLinkReturn extends Omit<NextLinkProps, 'href'> {
+  href: string
 }
