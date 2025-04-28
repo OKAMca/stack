425c2b7a780874bab24255f54a77a1047689dc17
Marie-Maxime Tanguay <marie-maxime@okam.ca>
2025-04-28 11:23:48 -0700

    feat: add header option to select component

--- libs/stack/stack-ui/src/components/fields/Select/components/Listbox.tsx
+++ libs/stack/stack-ui/src/components/fields/Select/components/Listbox.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { FocusScope, useListBox, useOption } from 'react-aria'
 import useThemeContext from '../../../../providers/Theme/hooks'
-import { TypographyWithForwardRef } from '../../../Typography'
+import Box, { BoxWithForwardRef } from '../../../Box'
+import Typography, { TypographyWithForwardRef } from '../../../Typography'
 import type { TListBoxProps, TOptionProps } from './Listbox.interface'
+import { useListboxSections } from './useListboxSections'
 
 const Option = ({ item, state, themeName = 'li' }: TOptionProps) => {
   const ref = React.useRef(null)
@@ -21,19 +23,65 @@ const Option = ({ item, state, themeName = 'li' }: TOptionProps) => {
   )
 }
 
+const InnerListBox = (props: {
+  children: React.ReactNode
+  listBoxRef: React.RefObject<HTMLUListElement>
+  themeName: string | null
+}) => {
+  const { children, listBoxRef, themeName } = props
+  return (
+    <FocusScope autoFocus restoreFocus contain>
+      <BoxWithForwardRef {...props} ref={listBoxRef} as="ul" themeName={`${themeName}.ul`}>
+        {children}
+      </BoxWithForwardRef>
+    </FocusScope>
+  )
+}
+
 const ListBox = (props: TListBoxProps) => {
   const ref = React.useRef(null)
   const { listBoxRef = ref, state, themeName = 'option' } = props
   const { listBoxProps } = useListBox(props, state, listBoxRef)
+  const collection = [...state.collection] as { key: string; rendered: string }[]
 
-  return (
-    <FocusScope autoFocus restoreFocus contain>
-      <TypographyWithForwardRef {...listBoxProps} ref={listBoxRef} as="ul" themeName={`${themeName}.ul`}>
+  const sections = useListboxSections(collection)
+  const hasHeaders = sections.some((section) => section.header?.key != null)
+
+  if (!hasHeaders) {
+    return (
+      <InnerListBox {...listBoxProps} listBoxRef={listBoxRef} themeName={themeName}>
         {[...state.collection].map((item) => (
           <Option themeName={`${themeName}.li`} key={item.key} item={item} state={state} />
         ))}
-      </TypographyWithForwardRef>
-    </FocusScope>
+      </InnerListBox>
+    )
+  }
+
+  return (
+    <InnerListBox {...listBoxProps} listBoxRef={listBoxRef} themeName={themeName}>
+      {sections.map((section) => {
+        const sectionKey = section.header
+          ? `section-${section.header.key}`
+          : `section-${section.items[0]?.key || 'empty'}`
+
+        return (
+          <Box key={sectionKey} as="li" themeName={`${themeName}.group`}>
+            <Box as="section" themeName={`${themeName}.section`}>
+              {section.header && (
+                <Typography as="header" themeName={`${themeName}.headerText`} key={section.header.key}>
+                  {section.header.rendered}
+                </Typography>
+              )}
+              <Box as="ul" themeName={`${themeName}.list`}>
+                {section.items.map((item) => (
+                  <Option themeName={`${themeName}.li`} key={item.key} item={item} state={state} />
+                ))}
+              </Box>
+            </Box>
+          </Box>
+        )
+      })}
+    </InnerListBox>
   )
 }
 
