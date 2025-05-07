edfeb2013b1042f176d122c697a2f01b80093b80
poclerson <pierre-olivier@okam.ca>
2025-05-07 15:18:53 -0400

    feat(stack-ui): tablist is fully themeable

---
+++ libs/stack/stack-ui/src/components/TabList/components/TabPanel.tsx
@@ -0,0 +1,35 @@
+import { useTabPanel } from '@react-aria/tabs'
+import { useRef } from 'react'
+import type { TToken } from '../../../providers/Theme/interface'
+import RenderWithOpacity from '../../../transitions/RenderWithOpacity'
+import { BoxWithForwardRef } from '../../Box'
+import type { TTabPanelProps } from '../interface'
+
+const TabPanel = <I extends object, T extends TToken = TToken>(props: TTabPanelProps<I, T>) => {
+  const {
+    state,
+    customTheme,
+    themeName = 'tabList.panel',
+    tokens,
+    panelTransition: PanelTransition = RenderWithOpacity,
+    ...rest
+  } = props
+  const ref = useRef(null)
+  const { tabPanelProps } = useTabPanel(props, state, ref)
+
+  return (
+    <PanelTransition
+      {...rest}
+      themeName={`${themeName}.transition`}
+      tokens={tokens}
+      customTheme={customTheme}
+      isVisible
+    >
+      <BoxWithForwardRef themeName={`${themeName}.container`} tokens={tokens} {...tabPanelProps} ref={ref}>
+        {state.selectedItem?.props.children}
+      </BoxWithForwardRef>
+    </PanelTransition>
+  )
+}
+
+export default TabPanel
