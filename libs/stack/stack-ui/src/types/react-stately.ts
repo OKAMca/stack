1f73128fdd4c62d73b0a0408a6cac1e95eb64fc5
poclerson <pierre-olivier@okam.ca>
2025-05-07 15:19:51 -0400

    fix(stack-ui): tabs can receive props from both tablist mapping and tabitems props

---
+++ libs/stack/stack-ui/src/types/react-stately.ts
@@ -0,0 +1,13 @@
+import type { ItemProps, Node, Key } from 'react-stately'
+import type { TToken } from '../providers/Theme/interface'
+import type { TDefaultComponent } from './components'
+
+export interface TDefaultNode<I extends object> extends Omit<Node<I>, 'props'> {
+  props?: TDefaultComponent & I
+}
+
+export interface TDefaultItemComponent<I extends object = object, T = TToken>
+  extends Omit<TDefaultComponent<T>, 'children'>,
+    ItemProps<I> {
+  key: Key
+}
