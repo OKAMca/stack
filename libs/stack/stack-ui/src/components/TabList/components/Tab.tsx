f055a122086386faa79a72e740b3f8631901ee9b
poclerson <pierre-olivier@okam.ca>
2025-05-08 16:08:47 -0400

    fix(stack-ui): tab titles are clickable

--- libs/stack/stack-ui/src/components/TabList/components/Tab.tsx
+++ libs/stack/stack-ui/src/components/TabList/components/Tab.tsx
@@ -3,21 +3,15 @@
 import { useFocusRing } from '@react-aria/focus'
 import { useTab } from '@react-aria/tabs'
 import { useRef } from 'react'
+import { mergeProps } from 'react-aria'
 import { mergeDefaultComponentProps } from '../../../helpers/mergeDefaultComponentProps'
 import type { TDefaultItemComponent } from '../../../types/react-stately'
 import { BoxWithForwardRef } from '../../Box'
-import Button from '../../Button'
 import type { TTabProps } from '../interface'
 
 const Tab = <I extends TDefaultItemComponent>({ item, state, ...props }: TTabProps<I>) => {
   const { key, rendered } = item
-  const {
-    themeName = 'tabList.tab',
-    tokens,
-    customTheme,
-    as: Component = Button,
-    ...rest
-  } = mergeDefaultComponentProps(props, item.props)
+  const { themeName = 'tabList.tab', tokens, ...rest } = mergeDefaultComponentProps(props, item.props)
 
   const ref = useRef(null)
   const { tabProps } = useTab({ key }, state, ref)
@@ -37,17 +31,8 @@ const Tab = <I extends TDefaultItemComponent>({ item, state, ...props }: TTabPro
   }
 
   return (
-    <BoxWithForwardRef
-      {...tabProps}
-      {...focusProps}
-      themeName={`${themeName}.container`}
-      tokens={tabTokens}
-      customTheme={customTheme}
-      ref={ref}
-    >
-      <Component {...rest} themeName={`${themeName}.button`} tokens={tabTokens}>
-        {rendered}
-      </Component>
+    <BoxWithForwardRef {...mergeProps(rest, focusProps, tabProps)} themeName={themeName} tokens={tabTokens} ref={ref}>
+      {rendered}
     </BoxWithForwardRef>
   )
 }
