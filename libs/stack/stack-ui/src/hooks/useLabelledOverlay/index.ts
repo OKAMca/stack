11f8756e4f15167679114883420f173fb12ef988
poclerson <pierre-olivier@okam.ca>
2025-05-22 12:05:43 -0400

    docs(stack-ui): documented/explicit types for useLabelledOverlay

--- libs/stack/stack-ui/src/hooks/useLabelledOverlay/index.ts
+++ libs/stack/stack-ui/src/hooks/useLabelledOverlay/index.ts
@@ -7,28 +7,32 @@ import type { RefObject } from 'react'
 import type { OverlayTriggerProps } from 'react-aria'
 import { useOverlayTrigger } from 'react-aria'
 import type { OverlayTriggerState } from 'react-stately'
+import type { TLabelledOverlayAria } from './interface'
 
-function useLabelledOverlay(props: OverlayTriggerProps, state: OverlayTriggerState, ref?: RefObject<Element>) {
+/**
+ * Wrapper for react aria `useOverlayTrigger` that adds label props and reformats press events to work with `TButtonProps`
+ */
+function useLabelledOverlay(
+  props: OverlayTriggerProps,
+  state: OverlayTriggerState,
+  ref?: RefObject<Element>,
+): TLabelledOverlayAria {
   const { type = 'dialog' } = props
   const {
     triggerProps: { onPress, ...triggerProps },
     overlayProps,
   } = useOverlayTrigger({ type }, state, ref)
 
+  const labelId = useSlotId()
+
   const updatedTriggerProps = {
+    'aria-labelledby': labelId,
     handlePress: onPress,
     ...triggerProps,
   }
 
-  const labelId = useSlotId()
-
-  const openTriggerProps = {
-    'aria-labelledby': labelId,
-    ...updatedTriggerProps,
-  }
-
   return {
-    openTriggerProps,
+    openTriggerProps: updatedTriggerProps,
     triggerProps: updatedTriggerProps,
     labelProps: { id: labelId },
     overlayProps: { ...overlayProps, 'aria-labelledby': labelId },
