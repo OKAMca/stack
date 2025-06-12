0aa7aca2547fec5901905820dc257aa438c2dd54
poclerson <pierre-olivier@okam.ca>
2025-06-12 13:23:03 -0400

    refactor(stack-ui): defaultcollectionchild type -> defaultnode type

--- libs/stack/stack-ui/src/components/fields/ListBox/interface.ts
+++ libs/stack/stack-ui/src/components/fields/ListBox/interface.ts
@@ -3,12 +3,12 @@ import type { AriaListBoxProps, AriaListBoxSectionProps } from 'react-aria'
 import type { ListState } from 'react-stately'
 import type { TToken } from '../../../providers/Theme/interface'
 import type { TDefaultComponent } from '../../../types/components'
-import type { TDefaultCollectionChildComponent, TDefaultNode } from '../../../types/react-stately'
+import type { TDefaultNode, TDefaultNodeComponent } from '../../../types/react-stately'
 import type { TAnchorProps } from '../../Button/interface'
 
 export interface TListBoxProps<I extends object = object, T extends TToken = TToken>
   extends Omit<TDefaultComponent<T>, 'children'>,
-    AriaListBoxProps<I & TDefaultCollectionChildComponent<I, T>> {
+    AriaListBoxProps<I & TDefaultNodeComponent<I, T>> {
   /**
    * @default Anchor
    */
