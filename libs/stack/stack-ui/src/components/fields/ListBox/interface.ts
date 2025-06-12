1b1fa8116e072355015adc688ff5967ff811e745
poclerson <pierre-olivier@okam.ca>
2025-06-12 11:57:08 -0400

    fix(stack-ui): accept DOMAttrributes on Box

--- libs/stack/stack-ui/src/components/fields/ListBox/interface.ts
+++ libs/stack/stack-ui/src/components/fields/ListBox/interface.ts
@@ -3,16 +3,22 @@ import type { AriaListBoxProps, AriaListBoxSectionProps } from 'react-aria'
 import type { ListState } from 'react-stately'
 import type { TToken } from '../../../providers/Theme/interface'
 import type { TDefaultComponent } from '../../../types/components'
-import type { TDefaultItemComponent, TDefaultNode } from '../../../types/react-stately'
+import type { TDefaultCollectionChildComponent, TDefaultNode } from '../../../types/react-stately'
 import type { TAnchorProps } from '../../Button/interface'
 
 export interface TListBoxProps<I extends object = object, T extends TToken = TToken>
   extends Omit<TDefaultComponent<T>, 'children'>,
-    AriaListBoxProps<I & TDefaultItemComponent<I, T>> {
+    AriaListBoxProps<I & TDefaultCollectionChildComponent<I, T>> {
   /**
    * @default Anchor
    */
   linkElementType?: ForwardRefExoticComponent<TAnchorProps & RefAttributes<HTMLAnchorElement & HTMLButtonElement>>
+}
+
+export interface TControlledListBoxProps<I extends object = object, T extends TToken = TToken>
+  extends Omit<TListBoxProps<I, T>, 'children'>,
+    Partial<Pick<TListBoxProps<I, T>, 'children'>> {
+  state: ListState<I>
 }
 
 export interface TListBoxSectionProps<I extends object = object, T extends TToken = TToken>
