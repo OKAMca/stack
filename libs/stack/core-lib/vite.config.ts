fa9cde4cff6fbd9e96b134286c74085a6028ff38
poclerson <pierre-olivier@okam.ca>
2025-06-13 13:22:06 -0400

    fix(core-lib): added back cjs build format

--- libs/stack/core-lib/vite.config.ts
+++ libs/stack/core-lib/vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
-      formats: ['es'],
+      formats: ['es', 'cjs'],
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
