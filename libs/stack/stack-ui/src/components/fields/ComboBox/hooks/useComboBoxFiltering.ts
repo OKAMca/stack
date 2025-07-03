514bdccc17f49da4beab29f341c61814d5dfb5b5
Max Hilland <max.hilland@okam.ca>
2025-07-03 10:27:11 -0400

    build issue

--- libs/stack/stack-ui/src/components/fields/ComboBox/hooks/useComboBoxFiltering.ts
+++ libs/stack/stack-ui/src/components/fields/ComboBox/hooks/useComboBoxFiltering.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useMemo, useState } from 'react'
 import type { TDefaultSectionComponent, TDefaultItemComponent } from '../../../../types/react-stately'
 import { Node } from '../../../Node'
