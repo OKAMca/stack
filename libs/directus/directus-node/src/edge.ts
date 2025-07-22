ab65a2e0ebcc4fda0e34b63b3818e6369d9cc86f
poclerson <pierre-olivier@okam.ca>
2025-07-22 14:18:57 -0400

    docs(directus-node): deprecate edge-incompatible exports from index.ts

--- libs/directus/directus-node/src/edge.ts
+++ libs/directus/directus-node/src/edge.ts
@@ -2,6 +2,12 @@
  * @fileoverview Export file for functions that are used in edge functions (middleware-compatible)
  */
 
-export type { TFetchRedirectsConfig, TFetchRedirectsResponse, TRedirectData, TRedirectType } from './lib/redirection'
+export type {
+  TFetchRedirectsConfig,
+  TFetchRedirectsResponse,
+  TRedirectData,
+  TRedirectType,
+} from './lib/redirection/interface'
 
-export { fetchRedirectsData, getDefaultConfig, isRedirect, normalizeRedirects } from './lib/redirection'
+export { fetchRedirectsData, getDefaultConfig } from './lib/redirection/fetchRedirectsData'
+export { isRedirect, normalizeRedirects } from './lib/redirection/utils/validateRedirects'
--- libs/directus/directus-node/src/lib/redirection/index.ts
+++ libs/directus/directus-node/src/lib/redirection/index.ts
@@ -1,4 +1,20 @@
-export * from './redirectsFile'
-export * from './fetchRedirectsData'
+/* eslint-disable prefer-destructuring */
+
+import * as fetchRedirectsDataFile from './fetchRedirectsData'
+// Re-export with deprecation warnings
+/**
+ * @deprecated Import from `@okam/directus-node/edge` instead
+ */
+export const fetchRedirectsData = fetchRedirectsDataFile.fetchRedirectsData
+/**
+ * @deprecated Import from `@okam/directus-node/edge` instead
+ */
+export const getDefaultConfig = fetchRedirectsDataFile.getDefaultConfig
+/**
+ * @deprecated Import from `@okam/directus-node/edge` instead
+ */
+export const redirectDefaultLimit = fetchRedirectsDataFile.redirectDefaultLimit
+
 export type { TFetchRedirectsConfig, TFetchRedirectsResponse, TRedirectData, TRedirectType } from './interface'
 export * from './utils/validateRedirects'
+export * from './redirectsFile'
