d6dcdc50f675270291abf461ab8964aa4098616e
poclerson <pierre-olivier@okam.ca>
2025-08-06 12:04:25 -0400

    fix(directus-next): usePageSettings -> getPageSettings

--- libs/directus/directus-next/src/pageSettings/getPageSettings.ts
+++ libs/directus/directus-next/src/pageSettings/getPageSettings.ts
@@ -5,24 +5,20 @@ import type { Variables } from 'graphql-request'
 import { get, invert, isEqual } from 'radashi'
 import { log } from '../logger'
 import type { DirectusRouteConfig } from '../types/directusRouteConfig'
+import type { PageSettingsQueryItem } from '../types/pageSettings'
 import { pageSettingsContext, pageSettingsVariablesContext } from './context'
-import type {
-  TGetPageSettingsConfig,
-  TGetPageSettingsProps,
-  TPageSettingsQueryItem,
-  TGetPageSettingsReturn,
-} from './interface'
+import type { GetPageSettingsConfig, GetPageSettingsProps, GetPageSettingsReturn } from './interface'
 
 const [getPageSettingsContext, setPageSettingsContext] = pageSettingsContext()
 const [getVariables, setVariables] = pageSettingsVariablesContext()
 
-function isDirectusRouteConfig(config: TGetPageSettingsConfig | undefined): config is DirectusRouteConfig {
+function isDirectusRouteConfig(config: GetPageSettingsConfig | undefined): config is DirectusRouteConfig {
   return !!config && 'localeMap' in config
 }
 
 function getDirectusVariables<QueryVariables extends Variables>(
   variables: QueryVariables | undefined,
-  config: TGetPageSettingsConfig | undefined,
+  config: GetPageSettingsConfig | undefined,
 ) {
   const localeMap = isDirectusRouteConfig(config) ? config.localeMap : config
   if (!localeMap) {
@@ -61,16 +57,16 @@ function getDirectusVariables<QueryVariables extends Variables>(
  * @returns The new queried page settings item or the cached value if the variables have not changed. If the query contains a fragment, the contents of the fragment will be returned.
  */
 export async function getPageSettings<
-  Item extends TPageSettingsQueryItem,
+  Item extends PageSettingsQueryItem,
   ItemKey extends string = string,
   QueryVariables extends Variables = Variables,
 >(
-  props?: TGetPageSettingsProps<Item, ItemKey, QueryVariables>,
+  props?: GetPageSettingsProps<Item, ItemKey, QueryVariables>,
   itemKey?: Exclude<ItemKey, '__typename'>,
-): Promise<TGetPageSettingsReturn<Item>> {
+): Promise<GetPageSettingsReturn<Item>> {
   const { variables, config } = props ?? {}
   const directusVariables = getDirectusVariables(variables, config)
-  const defaultReturn = (getPageSettingsContext() as TGetPageSettingsReturn<Item>) ?? {}
+  const defaultReturn = getPageSettingsContext() as Exclude<GetPageSettingsReturn<Item>, undefined>
 
   if (!props || isEqual(getVariables(), directusVariables)) {
     log('Using cached page settings', { path: defaultReturn.page_settings?.translations?.[0]?.path })
@@ -84,7 +80,7 @@ export async function getPageSettings<
   const result = await queryGql(document, directusVariables)
 
   const items = result?.[key]
-  const currentItem = (Array.isArray(items) ? items?.[0] : items) as TGetPageSettingsReturn<Item>
+  const currentItem = (Array.isArray(items) ? items?.[0] : items) as GetPageSettingsReturn<Item>
   const currentPageSettings = currentItem?.page_settings
   const currentPath = currentPageSettings?.translations?.[0]?.path
 
@@ -104,3 +100,8 @@ export async function getPageSettings<
 
   return currentItem
 }
+
+/**
+ * @deprecated Use `getPageSettings` instead
+ */
+export const usePageSettings = getPageSettings
--- libs/directus/directus-next/src/types/pageSettings.ts
+++ libs/directus/directus-next/src/types/pageSettings.ts
@@ -1,20 +1,40 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+import type { Nullable } from '@okam/stack-ui'
+import type { Fragmentize } from './Fragments'
+
 export interface PageSettingsTranslation {
-  languages_code?:
-    | {
-        code?: string | null | undefined
-      }
-    | null
-    | undefined
-  id: string
-  page_settings_id?:
-    | {
-        belongs_to_collection?: string | null | undefined
-        belongs_to_key?: string | null | undefined
-      }
-    | null
-    | undefined
-  title?: string | null | undefined
-  slug?: string | null | undefined
-  path?: string | null | undefined
+  languages_code?: Nullable<{
+    code?: Nullable<string>
+  }>
+  page_settings_id?: Nullable<{
+    belongs_to_collection?: Nullable<string>
+    belongs_to_key?: Nullable<string>
+  }>
+  title?: Nullable<string>
+  slug?: Nullable<string>
+  path?: Nullable<string>
 }
+
+export interface PageSettings {
+  id: string
+  belongs_to_collection?: Nullable<string>
+  belongs_to_key?: Nullable<string>
+  translations?: DeepNullableArray<PageSettingsTranslation>
+  route?: Nullable<{
+    translations?: DeepNullableArray<{ route?: Nullable<string> }>
+  }>
+}
+
+export type PageSettingsQueryItem = Nullable<{
+  page_settings?: PageSettings | Fragmentize<PageSettings, 'PageSettingsFragment'>
+}>
+
+export type PageSettingsItemQuery<Item extends PageSettingsQueryItem, ItemKey extends string> = {
+  [Key in ItemKey]?: MaybeArray<Item> | MaybeArray<Fragmentize<Item>>
+} & {
+  __typename?: 'Query'
+}
+
+export type DeepNullableArray<T> = Nullable<Nullable<T>[]>
+
+export type MaybeArray<T> = T | (T | null | undefined)[] | null | undefined
