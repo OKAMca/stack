37933fc48f5826d3c35bfd00f425650397f1f871
poclerson <pierre-olivier@okam.ca>
2025-08-08 09:50:29 -0400

    docs(demo): next docs forrr useFilterState/Filter

---
+++ apps/demo/app/next-component/filter/page.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@okam/stack-ui'
+import FruitFilter from './components/ColorFilter'
+
+export default async function Page() {
+  return (
+    <Box>
+      <FruitFilter />
+    </Box>
+  )
+}
