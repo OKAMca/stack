26e9b89ced47744c5d18bf153e9471fecafdb80e
poclerson <pierre-olivier@okam.ca>
2025-08-08 09:50:06 -0400

    docs(next-component): filter component stories

---
+++ libs/stack/next-component/src/theme/Button/index.ts
@@ -0,0 +1,76 @@
+import { tv } from 'tailwind-variants'
+
+const button = tv({
+  base: `
+    flex
+    items-center
+    justify-center
+    gap-4
+    transition
+    duration-300
+    ease-in-out
+    disabled:pointer-events-none
+    disabled:opacity-30
+    focus-ring-black
+  `,
+  defaultVariants: {
+    buttonStyle: 'default',
+    type: 'button',
+    size: 'default',
+    shape: 'rounded',
+  },
+  variants: {
+    type: {
+      button: '',
+      menu: 'text-2xl',
+    },
+    buttonStyle: {
+      default: `
+            px-4
+            py-2
+            text-white
+            !bg-color-1-500
+            hover:!bg-color-1-400
+            active:!bg-color-1-400
+          `,
+      outline: `
+            px-4
+            py-2
+            bg-transparent
+            !border-color-1-500
+            border-2
+            text-color-1-500
+            hover:bg-color-1-500
+            hover:text-white
+            active:bg-color-1-500
+            active:text-white
+          `,
+      hollow: `
+            px-2
+            bg-transparent
+            text-color-1-500
+            hover:border-b-color-1-500
+            active:border-b-color-1-500
+            focus:border-b-color-1-500
+          `,
+    },
+    intent: {
+      error: `
+            !bg-error
+            text-white
+            pointer-events-none
+            !border-error
+          `,
+    },
+    size: {
+      default: `min-w-12 min-h-6`,
+      large: `min-w-36 min-h-18`,
+    },
+    shape: {
+      rounded: `rounded-md`,
+      circular: `rounded-full`,
+    },
+  },
+})
+
+export default button
