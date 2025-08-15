82edd0a48a968d1904fe1810fb794df1ac7db6fd
poclerson <pierre-olivier@okam.ca>
2025-08-15 12:50:23 -0400

    docs(next-component): next demo example query + component

--- apps/demo/app/[locale]/Providers.tsx
+++ apps/demo/app/[locale]/Providers.tsx
@@ -1,10 +1,17 @@
 'use client'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { I18nProvider } from 'react-aria'
+
+const queryClient = new QueryClient()
 
 const Providers = (props: { children: React.ReactNode; locale: string }) => {
   const { children, locale } = props
-  return <I18nProvider locale={locale}>{children}</I18nProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <I18nProvider locale={locale}>{children}</I18nProvider>
+    </QueryClientProvider>
+  )
 }
 
 export default Providers
