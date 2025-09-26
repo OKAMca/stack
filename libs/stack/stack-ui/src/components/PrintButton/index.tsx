50d3828254d915811907191cf8b7e860efb426a8
Max Hilland <max.hilland@okam.ca>
2025-09-26 15:34:14 -0400

    fix(stack-ui): add window check

--- libs/stack/stack-ui/src/components/PrintButton/index.tsx
+++ libs/stack/stack-ui/src/components/PrintButton/index.tsx
@@ -7,12 +7,18 @@ import type { TPrintButtonProps } from './interface'
 const PrintButton = (props: TPrintButtonProps) => {
   const { ariaLabel, icon, customTheme, themeName = 'printButton', tokens, ...rest } = props
 
+  const handleClick = () => {
+    if (typeof window !== 'undefined' && typeof window.print === 'function') {
+      window.print()
+    }
+  }
+
   return (
     <Button
       themeName={`${themeName}.button`}
       tokens={tokens}
       aria-label={ariaLabel}
-      handlePress={() => window.print()}
+      handlePress={handleClick}
       {...rest}
     >
       <Icon themeName={`${themeName}.icon`} icon={icon ?? 'Print'} />
