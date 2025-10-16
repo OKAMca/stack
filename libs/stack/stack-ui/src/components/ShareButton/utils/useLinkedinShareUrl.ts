bb44c4c2f048f410b5bf29a23c616b5428db4e75
Max Hilland <max.hilland@okam.ca>
2025-10-16 09:36:19 -0500

    fix(stack-ui): use correct operator

--- libs/stack/stack-ui/src/components/ShareButton/utils/useLinkedinShareUrl.ts
+++ libs/stack/stack-ui/src/components/ShareButton/utils/useLinkedinShareUrl.ts
@@ -6,7 +6,7 @@ import useWindow from './useWindow'
 const useLinkedinShareUrl = (media: string, medium: string, urlToShare: string) => {
   const { windowLocation } = useWindow()
 
-  const currentURL = urlToShare ?? windowLocation
+  const currentURL = urlToShare || windowLocation
   const utm = generateUtmTags(media, medium)
   const fullUrl = `${currentURL}${utm}`
   return `https://www.linkedin.com/shareArticle?mini=true&url=${fullUrl}`
