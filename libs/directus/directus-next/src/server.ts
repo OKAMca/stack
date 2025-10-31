563bbc0c3623f5bf19aecf22709db888be368e27
poclerson <pierre-olivier@okam.ca>
2025-10-31 13:47:24 -0400

    fix(directus-next): local use of createServerContext, uninstalled server-only-context

--- libs/directus/directus-next/src/server.ts
+++ libs/directus/directus-next/src/server.ts
@@ -1,3 +1,4 @@
 export { directusRouteRouter } from './router/router'
 export type { DirectusRouteConfig } from './types/directusRouteConfig'
 export * from './pageSettings'
+export { createServerContext } from './createServerContext'
