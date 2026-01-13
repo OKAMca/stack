bdb597910ebf21a385bbcf467e9064e06c88479e
poclerson <pierre-olivier@okam.ca>
2026-01-13 12:46:20 -0500

    fix(next-component): allow passing options to draft provider query/mutation

--- libs/stack/next-component/src/providers/DraftMode/interface.ts
+++ libs/stack/next-component/src/providers/DraftMode/interface.ts
@@ -1,15 +1,23 @@
-import type { UseMutationResult } from '@tanstack/react-query'
+import type { UseMutationOptions, UseMutationResult, UseQueryOptions } from '@tanstack/react-query'
 import type { ReactNode } from 'react'
 
 export interface TDraftModeProviderProps {
   children: ReactNode
   defaultEnabled?: boolean
-  cookieDuration?: number // Duration in days
+  /**
+   * The duration of the draft mode cookie in days
+   */
+  cookieDuration?: number
+  queryOptions?: UseQueryOptions<TDraftModeStatus, Error, TDraftModeStatus, [boolean, number]>
+  mutationOptions?: UseMutationOptions<TDraftModeStatus, Error, TDraftModeStatus, unknown>
 }
 
 export type TDraftModeStatus = {
   isEnabled: boolean
-  cookieDuration?: number // Duration in days
+  /**
+   * The duration of the draft mode cookie in days
+   */
+  cookieDuration?: number
 }
 
 type TDraftModeQueryResult = UseMutationResult<TDraftModeStatus, Error, TDraftModeStatus, unknown>
