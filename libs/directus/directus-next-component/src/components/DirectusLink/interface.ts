fcfd427b2bd3d57c79ba24c32d93f0fcfada29ae
poclerson <pierre-olivier@okam.ca>
2026-01-13 11:17:55 -0500

    fix(directus-next-component): typename typo

--- libs/directus/directus-next-component/src/components/DirectusLink/interface.ts
+++ libs/directus/directus-next-component/src/components/DirectusLink/interface.ts
@@ -5,16 +5,16 @@ import type { TLinks } from '../../types/links'
 export type DirectusLink = Omit<TDefaultComponent, 'children'> & Omit<Partial<TLinks>, 'tokens'> & AriaAttributes
 
 export interface TGetDirectusLink extends DirectusLink {
-  propsConfig?: TTDirectusLinkPropsConfig
+  propsConfig?: TDirectusLinkPropsConfig
 }
 export interface TDirectusLinkProps extends DirectusLink {
   componentsConfig?: TDirectusLinkComponentsConfig
-  propsConfig?: TTDirectusLinkPropsConfig
+  propsConfig?: TDirectusLinkPropsConfig
 }
 
 export type TDirectusLinkComponentsConfig = Record<string, ComponentType<TDirectusLinkProps>>
 
-export type TTDirectusLinkPropsConfig<ReturnProps extends TAnchorProps = TAnchorProps> = Record<
+export type TDirectusLinkPropsConfig<ReturnProps extends TAnchorProps = TAnchorProps> = Record<
   string,
   (props: TGetDirectusLink) => ReturnProps
 >
