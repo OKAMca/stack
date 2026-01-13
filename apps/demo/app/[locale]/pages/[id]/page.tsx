da0114578a88ee209b12d8abf33a29bf7dcb03e7
poclerson <pierre-olivier@okam.ca>
2026-01-13 11:18:11 -0500

    fix(demo): typo

--- apps/demo/app/[locale]/pages/[id]/page.tsx
+++ apps/demo/app/[locale]/pages/[id]/page.tsx
@@ -11,7 +11,7 @@ export default async function Page(props: { params: Promise<{ id: string; locale
   })
 
   // If the query has no main fragment, the type parameters must be passed.
-  // const dataWithoutFragment = await GetPageSettings<PageFragment, 'pages_by_id', typeof props.params>({
+  // const dataWithoutFragment = await getPageSettings<PageFragment, 'pages_by_id', typeof props.params>({
   //   document: PageByIdWithoutFragmentDocument,
   //   variables: props.params,
   //   config: directusConfig,
