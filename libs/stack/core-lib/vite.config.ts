4e337b81ead5c57fd21715285d571c0e4fa3dd77
poclerson <pierre-olivier@okam.ca>
2026-01-16 11:49:24 -0500

    fix(core-lib): stop bundling okam libs

--- libs/stack/core-lib/vite.config.ts
+++ libs/stack/core-lib/vite.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: externalDeps,
+      external: [...externalDeps, /^@okam\//],
     },
   },
 })
