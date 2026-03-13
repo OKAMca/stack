60a6cce830c11319f660ad5e5052020f4c075545
poclerson <pierre-olivier@okam.ca>
2026-03-13 09:05:33 -0400

    docs(directus-next): add examples for locale prefix options

--- libs/directus/directus-next/src/types/directusRouteConfig.ts
+++ libs/directus/directus-next/src/types/directusRouteConfig.ts
@@ -16,10 +16,50 @@ export interface TDirectusRouteRedirectsModule {
 export enum DirectusRouteLocalePrefix {
   /**
    * The pathname will be prefixed with the locale only when it is not the default locale
+   *
+   * @example
+   * ```ts
+   * const directusConfig = {
+   *   defaultLocale: 'en-CA',
+   *   localeMap: {
+   *     'fr-CA': 'fr',
+   *     'en-CA': 'en',
+   *   }
+   * }
+   *
+   * // navigate to /en/products/1
+   * // output: /products/1 with english language
+   *
+   * // navigate to /products/1
+   * // output: /products/1 with english language
+   *
+   * // navigate to /fr/produits/1
+   * // output: /fr/produits/1 with french language
+   * ```
    */
   AsNeeded = 'as-needed',
   /**
    * The pathname will always be prefixed by the locale
+   *
+   * @example
+   * ```ts
+   * const directusConfig = {
+   *   defautLocale: 'en-CA',
+   *   localeMap: {
+   *     'fr-CA': 'fr',
+   *     'en-CA': 'en',
+   *   }
+   * }
+   *
+   * // navigate to /en/products/1
+   * // output: /en/products/1 with english language
+   *
+   * // navigate to /products/1
+   * // output: /products/1 with english language
+   *
+   * // navigate to /fr/produits/1
+   * // output: /fr/produits/1 with french language
+   * ```
    */
   Always = 'always',
 }
