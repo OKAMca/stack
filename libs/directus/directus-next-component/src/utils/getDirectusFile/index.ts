0e54310d141243c539efbc154cd3d145b987c221
poclerson <pierre-olivier@okam.ca>
2026-04-09 15:23:00 -0400

    docs(directus-next-component): grammar

--- libs/directus/directus-next-component/src/utils/getDirectusFile/index.ts
+++ libs/directus/directus-next-component/src/utils/getDirectusFile/index.ts
@@ -10,7 +10,7 @@ const IMG_PROTOCOL = process.env.NEXT_PUBLIC_IMG_PROTOCOL ?? 'https'
 /**
  * Creates a Directus asset URL from a Directus file
  * @param file Directus file to create the asset URL from
- * @param baseUrl Override Directus asset URL creation. By default, this will be fallbacked to with `NEXT_PUBLIC_IMG_DOMAIN`, `NEXT_PUBLIC_IMG_PORT` and `NEXT_PUBLIC_IMG_PROTOCOL`
+ * @param baseUrl Override Directus asset URL creation. By default, this falls back to `NEXT_PUBLIC_IMG_DOMAIN`, `NEXT_PUBLIC_IMG_PORT` and `NEXT_PUBLIC_IMG_PROTOCOL`
  * @param searchParams Additional search params to append to the URL
  * @returns Directus asset URL
  */
@@ -50,7 +50,7 @@ export function getDirectusUrl(file: Nullable<TFiles>, baseUrl?: URL, searchPara
 /**
  * Wrapper for {@link getDirectusUrl} to format a Directus file into `next/image` `Image` props
  * @param file Directus file to create the asset URL from
- * @param baseUrl Override Directus asset URL creation. By default, this will be fallbacked to with `NEXT_PUBLIC_IMG_DOMAIN`, `NEXT_PUBLIC_IMG_PORT` and `NEXT_PUBLIC_IMG_PROTOCOL`
+ * @param baseUrl Override Directus asset URL creation. By default, this falls back to `NEXT_PUBLIC_IMG_DOMAIN`, `NEXT_PUBLIC_IMG_PORT` and `NEXT_PUBLIC_IMG_PROTOCOL`
  * @param searchParams Additional search params to append to the URL
  * @returns Props ready to be passed to `next/image` `Image` component
  */
