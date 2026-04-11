5030f943d33bbbfcb9fbad4a781215b55754fad9
poclerson <pierre-olivier@okam.ca>
2026-04-11 10:26:23 -0400

    fix(directus-next-component): prevent unwanted props from leaking into element render

--- libs/directus/directus-next-component/src/components/DirectusImg/index.tsx
+++ libs/directus/directus-next-component/src/components/DirectusImg/index.tsx
@@ -1,15 +1,16 @@
 import type { CSSProperties } from 'react'
 import type { TDirectusImgProps } from './interface'
 import { Img } from '@okam/next-component'
+import { isNullish, omit } from 'radashi'
 import { createPngDataUri } from 'unlazy/thumbhash'
 import { getDirectusFile } from '../../utils/getDirectusFile'
 
-function DirectusImg(props: TDirectusImgProps) {
-  const { fit, baseUrl, focal_point_x: focalPointX, focal_point_y: focalPointY, thumbhash, width, height } = props
-
-  const fileProps = getDirectusFile(props, baseUrl, { fit })
-  if (fileProps == null)
+function DirectusImg({ baseUrl, ...rest }: TDirectusImgProps) {
+  const fileProps = getDirectusFile(rest, baseUrl, { fit: rest.fit })
+  if (isNullish(fileProps))
     return null
+
+  const { focal_point_x: focalPointX, focal_point_y: focalPointY, thumbhash, width, height } = fileProps
 
   const hasFocalPoint = focalPointX != null && focalPointX !== 0 && focalPointY != null && focalPointY !== 0 && width != null && width !== 0 && height != null && height !== 0
 
@@ -24,7 +25,7 @@ function DirectusImg(props: TDirectusImgProps) {
     <Img
       style={style}
       {...(thumbhash != null && thumbhash !== '' ? { blurDataURL: createPngDataUri(thumbhash), placeholder: 'blur' } : {})}
-      {...fileProps}
+      {...omit(fileProps, ['filenameDownload', 'duration', 'caption', 'embed', 'filenameDownload', 'filename_disk', 'filename_download', 'focal_point_divider', 'focal_point_x', 'focal_point_y', 'location', 'storage', 'tags', 'thumbhash', 'type'])}
     />
   )
 }
