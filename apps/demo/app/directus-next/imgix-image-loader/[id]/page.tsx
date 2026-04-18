a4331d75c589e116e68ae174f74a1e7813756d79
poclerson <pierre-olivier@okam.ca>
2026-04-18 11:48:42 -0400

    docs(demo): more generic filename download in imgix example

--- apps/demo/app/directus-next/imgix-image-loader/[id]/page.tsx
+++ apps/demo/app/directus-next/imgix-image-loader/[id]/page.tsx
@@ -7,15 +7,15 @@ export default async function Page({ params}: { params: Promise<{ id: string }> 
     <div>
       <div className="relative w-125 h-125">
         <span>Default image</span>
-        <DirectusImg id={id} filename_download="Ligue-Frisbee.png" width={1000} height={1000} />
+        <DirectusImg id={id} filename_download="image.png" width={1000} height={1000} />
       </div>
       <div className="relative w-125 h-125">
         <span>Focal point</span>
-        <DirectusImg id={id} filename_download="Ligue-Frisbee.png" width={1000} height={1000} focal_point_x={1000} focal_point_y={1000} fit="cover" />
+        <DirectusImg id={id} filename_download="image.png" width={1000} height={1000} focal_point_x={1000} focal_point_y={1000} fit="cover" />
       </div>
       <div className="relative w-125 h-125">
         <span>Fill</span>
-        <DirectusImg id={id} filename_download="Ligue-Frisbee.png" fill fit="cover" />
+        <DirectusImg id={id} filename_download="image.png" fill fit="cover" />
       </div>
     </div>
   )
