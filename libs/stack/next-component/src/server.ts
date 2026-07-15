65ad3f8924bb6eae0da0191b30ad06f6e539ec3e
poclerson <pierre-olivier@okam.ca>
2026-07-15 14:03:02 -0400

    fix(next-component): locale prefix only gets exported from index.ts barrel

--- libs/stack/next-component/src/server.ts
+++ libs/stack/next-component/src/server.ts
@@ -1,5 +1,4 @@
 export { default as AdminBar } from './components/AdminBar'
-export { LocalePrefix } from './hooks/useLink/interface'
 export { createServerContext } from './lib/createServerContext'
 
 export { default as DraftModeServerContextProvider } from './providers/DraftMode/server'
